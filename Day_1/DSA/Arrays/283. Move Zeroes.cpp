801a470f91a668c502edf27a34b93841e4c91bfa
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-11 13:16:22 +0530

    practice array prob

--- Day_1/DSA/Arrays/283. Move Zeroes.cpp
+++ Day_1/DSA/Arrays/283. Move Zeroes.cpp
@@ -19,3 +19,22 @@ public:
         nums = temp;
     }
 };
+
+// TC -> O(N)
+// SC -> O(1)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int pos = 0;
+
+        for(int i = 0;i <n;i++){
+            if(nums[i] != 0){
+                nums[pos++] = nums[i];
+            }
+        }
+
+        while(pos < n)
+            nums[pos++] = 0;
+    }
+};
