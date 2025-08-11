8f55ecb5b2d9d792a9a2046be61fa7a15f808c5a
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-11 13:03:19 +0530

    practice array prob

--- Day_1/DSA/Arrays/1. Two Sum.cpp
+++ Day_1/DSA/Arrays/1. Two Sum.cpp
@@ -20,3 +20,20 @@ public:
         return ans;
     }
 };
+
+// TC -> O(N)
+// SC -> O(N)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++) {
+            int rem = target - nums[i];
+            if (mpp.find(rem) != mpp.end()) {
+                return {mpp[rem], i};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {};
+    }
+};
