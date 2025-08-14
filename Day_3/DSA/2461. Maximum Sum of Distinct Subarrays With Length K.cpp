8cc5c38f8d7e915270a5e9a7d3a40bb504170a69
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-08-14 16:32:08 +0530

    practice array prob

---
+++ Day_3/DSA/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long ans = 0;
+        long long currWindowSum = 0;
+        int n = nums.size(), i = 0, j =0;
+        unordered_set<int> st;
+
+        while(j < n)
+        {
+            while(st.count(nums[j])){
+                currWindowSum -= nums[i];
+                st.erase(nums[i]);
+                i += 1;
+            }
+
+            currWindowSum += nums[j];
+            st.insert(nums[j]);
+
+            if(j - i + 1 == k){
+                ans = max(ans, currWindowSum);
+
+                currWindowSum -= nums[i];
+                st.erase(nums[i]);
+                i += 1;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
